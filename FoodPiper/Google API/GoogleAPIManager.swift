bed4c26bb5ecafc951706d1bf12147d66188973a
adeiji <adeiji@yahoo.com>
2015-10-16 15:08:07 -0700

    Initial Commit

---
+++ FoodPiper/Google API/GoogleAPIManager.swift
@@ -0,0 +1,15 @@
+//
+//  GoogleAPIManager.swift
+//  FoodPiper
+//
+//  Created by adeiji on 10/16/15.
+//  Copyright © 2015 Dephyned. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class GoogleAPIManager: NSObject {
+
+    // Call the Google Places API and get 
+}
---
+++ cloud_code/development/Food Piper/cloud/main.js
@@ -0,0 +1,37 @@
+// Future: Given a city it will call the Factual API and get all the restaurants in that city and store them in the Parse Database
+// Current: Go through all cities in the Location Table and get all the restaurants in those cities and store them in Parse
+
+Parse.Cloud.job("getRestaurants", function (request, response) {
+	var myLocation = Parse.Object.extend("Location");
+	var query = new Parse.Query(myLocation);
+	query.find({
+		success: function (results) {
+			alert("Successfully retrieved " + results.length + " cities.");		
+
+			for (var i = 0; i < results.length; i++) {
+				var location = results[i];
+				alert("Location is " + location);
+				var cityName = location.get("city");
+				cityName = cityName.replace(" ", "%20");
+				var url = 'http://api.v3.factual.com/t/restaurants-us?filters={%22locality%22:%20%22' + cityName +'%22}&include_count=true&KEY=MleIByZblcsN1V7TRLMh58AezBg5OvqT1EtZzKRM';
+				getRestaurantsFromFactualAPI(url);				
+			};
+		},
+		error: function (error) {
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
+
+	function getRestaurantsFromFactualAPI (url) {
+		console.log("getRestaurantsFromFactualAPI() called with url: " + url);
+		Parse.Cloud.httpRequest({
+			url: url
+		}).then(function (httpResponse) {
+			// Success
+			console.log(httpResponse.text);
+		}, function (httpResponse) {
+			// Error
+			console.error("Request failed with response code " + httpResponse.status);
+		});
+	}
+});
