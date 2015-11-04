595739c076da42976df4c880e3a5cdb06e70cf29
adeiji <adeiji@yahoo.com>
2015-11-04 13:20:27 -0800

    Made the map view look like in our mockups

---
+++ FoodPiper/MapView.swift
@@ -0,0 +1,23 @@
+//
+//  MapView.swift
+//  FoodPiper
+//
+//  Created by adeiji on 11/4/15.
+//  Copyright © 2015 Dephyned. All rights reserved.
+//
+
+import UIKit
+
+class MapView: UIView {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var mapView: GMSMapView!
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- FoodPiper/MapViewController.swift
+++ FoodPiper/MapViewController.swift
@@ -8,36 +8,52 @@
 
 import UIKit
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var currentLocation:CLLocation!
     var restaurants:Array<Restaurant>!
+    var mapView:MapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
         // Do any additional setup after loading the view.
         displayMapView()
     }
-
+    
+    override func viewDidAppear(animated: Bool) {
+        fitBoundsOfMapView(mapView.mapView)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    func getRandomColor () -> UIColor {
+        let hue = ( Double(arc4random()) % 256 / 256.0 );  //  0.0 to 1.0
+        let saturation = ( Double(arc4random()) % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+        let brightness = (Double(arc4random()) % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+        let color = UIColor(hue: CGFloat(hue), saturation: CGFloat(saturation), brightness: CGFloat(brightness), alpha: 1)
+        
+        return color
+    }
+    
     func displayMapView () {
-        let camera = GMSCameraPosition.cameraWithLatitude(currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, zoom: 10)
-        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
-        self.view = mapView
+        mapView = self.view as! MapView
+        let camera = GMSCameraPosition.cameraWithLatitude(currentLocation.coordinate.latitude, longitude: currentLocation.coordinate.longitude, zoom: 5)
         let marker = GMSMarker.init()
         marker.position = camera.target
         marker.appearAnimation = kGMSMarkerAnimationPop
-        marker.map = mapView
+        marker.map = mapView.mapView
         marker.title = "Current Location"
-        mapView.selectedMarker = marker
-        let markers = setMarkersForRestaurantsWithGoogleMap(mapView)
-        fitBoundsOfMapView(mapView, markers: markers)
-        
+        mapView.mapView.selectedMarker = marker
+        setMarkersForRestaurantsWithGoogleMap(mapView.mapView)
+        mapView.mapView = mapView.mapView;
     }
     
     func setMarkersForRestaurantsWithGoogleMap(mapView: GMSMapView) -> Array<GMSMarker>
@@ -48,7 +64,7 @@ class MapViewController: UIViewController {
             let marker = GMSMarker()
             marker.position = position
             marker.title = restaurant.name
-            marker.icon = GMSMarker.markerImageWithColor(UIColor.redColor())
+            marker.icon = GMSMarker.markerImageWithColor(getRandomColor())
             marker.map = mapView
             markers.append(marker)
         }
@@ -56,17 +72,33 @@ class MapViewController: UIViewController {
         return markers
     }
     
-    func fitBoundsOfMapView (mapView:GMSMapView, markers:Array<GMSMarker>) {
+    func fitBoundsOfMapView (mapView:GMSMapView) {
         let position = CLLocationCoordinate2DMake(currentLocation.coordinate.latitude, currentLocation.coordinate.longitude)
         var bounds = GMSCoordinateBounds.init(coordinate: position, coordinate: position)
-        for marker in markers {
-            bounds = bounds.includingCoordinate(marker.position)
+        for restaurant in restaurants {
+            let restaurantPosition = CLLocationCoordinate2DMake(restaurant.location.coordinate.latitude, restaurant.location.coordinate.longitude)
+            bounds = bounds.includingCoordinate(restaurantPosition)
         }
         
         mapView.animateWithCameraUpdate(GMSCameraUpdate.fitBounds(bounds, withPadding: 15.0))
     }
-    
 
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell.init(style: .Default, reuseIdentifier: "Cell")
+        cell.textLabel!.text = restaurants[indexPath.row].name
+        
+        return cell;
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return restaurants.count
+    }
+
+    
     /*
     // MARK: - Navigation
 
