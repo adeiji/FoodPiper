aa1e2267c326eaae2fc893647a82b517aa59292c
adeiji <adeiji@yahoo.com>
2015-11-24 18:21:48 -0800

    Adding filter for the category, just made it so that the user can filter by category and it display the restaurants on the view restaurants view

---
+++ FoodPiper/MessageTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageTableViewCell.swift
+//  FoodPiper
+//
+//  Created by adeiji on 11/20/15.
+//  Copyright © 2015 Dephyned. All rights reserved.
+//
+
+import UIKit
+
+class MessageTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
